c9ccf3d977be84fc446a9323e70d8d91c81873eb
Bismah Naeem <bianaeem18@gmail.com>
2024-04-23 00:45:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+//SHABANG
+import inquirer from "inquirer";
+//1st question
+let question = await inquirer.prompt([{
+        name: "sentence",
+        message: "enter your sentence to count the word",
+        type: "input"
+    }]);
+// create a variable     for removing  white space we use trim and  split for arranging words by using inverted commas " "
+const words = question.sentence.trim().split(" ");
+//printing an array of words to the console
+console.log(words);
+//print the word count of the sentence to the console
+console.log(`your sentence word count is ${words.length}`);
